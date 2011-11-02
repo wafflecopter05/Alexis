8955729785fb521208e7b4c44069e4c3683b9e97
Mike Kanaly <mrkanaly@gmail.com>
2011-11-02 13:45:38 -0700

    Basic UI with image background. Hard-coded login u:test pwd:password.

--- Android/Alexis/src/edison/alexis/AlexisActivity.java
+++ Android/Alexis/src/edison/alexis/AlexisActivity.java
@@ -1,13 +1,65 @@
 package edison.alexis;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class AlexisActivity extends Activity {
+public class AlexisActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
+	
+	private Button loginButton;
+	private EditText username, password;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       
+        //Set Layout to XML layout "main.xml"
         setContentView(R.layout.main);
+        
+        //Link XML object to variable
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        //Set loginButton to be listened for clicks
+        loginButton.setOnClickListener(this);
+        
+        //Link User/Pass fields to variables
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+      
     }
+
+    //Click Listener Method
+	@Override
+	public void onClick(View v) {
+		//Switch that gets the ID of the view that was clicked
+		switch(v.getId())
+		{
+		case R.id.loginButton:
+			if(checkCredentials())
+				{
+				TextView tv = new TextView(this);
+				tv.setText("Successfully Logged In");
+				setContentView(tv);
+				}
+			break;
+
+		}
+		
+	}
+	//Method checking credentials
+	public boolean checkCredentials()
+	{
+		if(username.getText().toString().equals("test") && password.getText().toString().equals("password"))
+			return true;
+		else
+			return false;
+	}
+	
+	
+    
 }
