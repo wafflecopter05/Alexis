f7274b5273b1ce9ab40d2510b41afdfd24b78745
Stevie Robinson <wafflecopter05@gmail.com>
2011-11-07 13:39:39 -0700

    Created PictureProblemViewController
    Different view controller for image-based problems. Maybe we could just
    condense them into a single View Controller, no matter what the type
    is? Will discuss.

---
+++ iOS/Alexis/PictureProblemViewController.h
@@ -0,0 +1,16 @@
+//
+//  PictureProblemViewController.h
+//  Alexis
+//
+//  Created by srrobin4 on 11/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PictureProblemViewController : UIViewController {
+    
+}
+
+@end
