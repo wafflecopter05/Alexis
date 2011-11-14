11e1be3465a184ac839ce128f8399898e528ede3
Blake Tucker <brtucker@asu.edu>
2011-11-14 14:37:14 -0700

    Added Window for User to select their problem type.
    Needs to be refined, but progress is being made.

---
+++ iOS/Alexis/UserChoiceController.h
@@ -0,0 +1,18 @@
+//
+//  UserChoiceController.h
+//  Alexis
+//
+//  Created by brtucker on 11/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UserChoiceController : UIViewController {
+    IBOutlet UITableView *tableView;
+}
+
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+@end
